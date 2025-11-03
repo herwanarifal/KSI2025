476da8e014d50413d9caae19b01d6707c6983264
herwanarifal <email_github_anda@contoh.com>
2025-11-03 14:48:11 +0700

    Menambahkan fitur koneksi ke database

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,1 @@
-<?php
-$koneksi = mysqli_connect("localhost", "root", "", "kampus");
-
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-} else {
-    echo "Koneksi berhasil!";
-}
-?>
+<?php echo 'Koneksi ke database berhasil'; ?>
